b654656ba1f189163f32016ce1f230bea92cb12e
Joao Ramalho <ramalho.vfc@gmail.com>
2025-06-27 21:01:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Enable Vulkan graphics
+The following script enabled Vulkan API graphics on supported Android devices, then force stops common applications in order for them to be re-opened using the new driver. Needs to be run every reboot.
+
+## Why?
+Vulkan offers better performance and better battery life on mobile phones. Models like Samsung S23 using OneUI 7.0 have been bundled with the driver but it's not active by default.
+
+## Revert
+Reboot the device
---
+++ vulcan.sh
@@ -0,0 +1,12 @@
+adb shell setprop debug.hwui.renderer skiavk
+adb shell am crash com.android.systemui
+adb shell am force-stop com.android.settings
+adb shell am force-stop com.sec.android.app.launcher
+adb shell am force-stop com.samsung.android.app.aodservice
+adb shell am crash com.google.android.inputmethod.latin b
+adb shell am force-stop com.whatsapp
+adb shell am force-stop com.samsung.android.messaging
+adb shell am force-stop com.android.chrome
+adb shell am force-stop com.android.beta
+adb shell am force-stop com.sec.android.app.camera
+adb shell am com.google.android.calendar
